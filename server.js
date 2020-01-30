71c53d98f5a8d9d69d5a03283371a03a109f49e7
i-lazarev <vanealazarev@gmail.com>
2020-01-30 11:44:44 +0100

    mongoose relation

--- server.js
+++ server.js
@@ -1,94 +1,125 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // connect to mongo db with all this strange options
 // so you do not get all these annyoing warnings on connecting
-mongoose.connect('mongodb://localhost/users_db', {
+mongoose.connect(
+  "mongodb://localhost/users_db",
+  {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
-}, (err) => {
+  },
+  err => {
     if (!err) {
-        console.log('MongoDB Connection succeeded')
+      console.log("MongoDB Connection succeeded");
     } else {
-        console.log('Error on DB connection: ' + err)
+      console.log("Error on DB connection: " + err);
     }
-});
+  }
+);
 
 // Create User Model
-const User = mongoose.model('User', new mongoose.Schema({
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     roles: [String]
-}));
+  })
+);
 
+const Todo = mongoose.model("Todo", new mongoose.Schema({
+    title:{type: String, required:true},
+    status:{type:String, enum:["OPEN", "IN PROCESS", "ON HOLD", "CANCELED"], default:"OPEN"},
+    user:{type:mongoose.Schema.Types.ObjectId, ref:"User"}
+}))
 // parse incoming JSON data (from fetch or browser client)
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/users/seed', (req, res) => {
-
-    // array of users to create
-    let users = [
-        {email: "admin@dci.de", password: "admin", roles: ['Admin']},
-        {email: "user1@dci.de", password: "pw1", roles: ['Guest']},
-        {email: "user2@dci.de", password: "pw2", roles: ['Reader', 'Editor']},
-    ]
-
-    // hash password for each user (using bcrypt)
-    let usersHashed = users.map(user => {
-        user.password = bcrypt.hashSync(user.password, 10)
-        return user
+app.get("/user/:id/todos/seed", (req, res,next)=>{
+    User.findById(req.params.id)
+    
+    .then(user =>{
+        if (!user) {
+            return next(`This user not exist`);
+          }
+        console.log(user)
+        Todo.create([
+            {title:"Wake up",
+             user:user._id},
+             {title:"Drink coffe",
+             status:"IN PROCESS",
+             user:user._id},
+             {title:"Go to Sleep",
+             status:"CANCELED",
+             user:user._id}
+    ])
+    .then(todo=>res.send(todo))
     })
 
-    // insert users into MongoDB
-    User.insertMany(usersHashed)
-    .then(usersNew => res.send(usersNew)) 
 })
+
+app.get("/users/seed", (req, res) => {
+  // array of users to create
+  let users = [
+    { email: "admin@dci.de", password: "admin", roles: ["Admin"] },
+    { email: "user1@dci.de", password: "pw1", roles: ["Guest"] },
+    { email: "user2@dci.de", password: "pw2", roles: ["Reader", "Editor"] }
+  ];
+
+  // hash password for each user (using bcrypt)
+  let usersHashed = users.map(user => {
+    user.password = bcrypt.hashSync(user.password, 10);
+    return user;
+  });
+
+  // insert users into MongoDB
+  User.insertMany(usersHashed).then(usersNew => res.send(usersNew));
+});
 
 // handle incoming LOGIN requests here....
-app.post('/login', (req, res, next) => {
+app.post("/login", (req, res, next) => {
+  // find user
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      // user with this email not found? => error
+      if (!user) {
+        return next(`Authentication failed`);
+      }
 
-    // find user
-    User.findOne({email: req.body.email}).then(user => {
-        // user with this email not found? => error
-        if(!user) {
-            return next(`Authentication failed`)
+      // compare passwords using bcrypt.compare() function
+      bcrypt.compare(req.body.password, user.password).then(success => {
+        // user password does not match password from login form? => error
+        if (!success) {
+          return next(`Authentication failed`);
         }
+        // create JWT token by signing
+        let secret = "jwt-master-secret";
+        let token = jwt.sign(
+          { id: user.id, email: user.email }, // WHAT data to sign
+          secret, // signing key
+          { expiresIn: "1h" } // expiry time
+        );
 
-        // compare passwords using bcrypt.compare() function
-        bcrypt.compare(req.body.password, user.password)
-        .then(success => {
-            // user password does not match password from login form? => error
-            if(!success) {
-                return next(`Authentication failed`)
-            }
-            // create JWT token by signing
-            let secret = "jwt-master-secret"
-            let token = jwt.sign(
-                {id: user.id, email: user.email}, // WHAT data to sign
-                secret, // signing key
-                { expiresIn: "1h" } // expiry time
-            )
-    
-            // return token
-            res.send({ token }) // => same as: { "token": token }
-        })
+        // return token
+        res.send({ token }); // => same as: { "token": token }
+      });
     })
-    .catch(err => next(err))
-})
+    .catch(err => next(err));
+});
 
-
-let port = 3000
+let port = 3000;
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}!`);
+  console.log(`Server listening on port ${port}!`);
 });
 
 //Run app, then load http://localhost:port in a browser to see the output.
